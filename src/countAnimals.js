ae05f51270122e7bb34b45c90aa907d71f7565b1
Rachel Tairum <rachel.tairum@gmail.com>
2022-01-06 20:48:29 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // seu código aqui
+  const todos = species.reduce((acc, elemento) => (
+    { ...acc, [elemento.name]: elemento.residents.length }), {});
+  if (animal === undefined) {
+    return todos;
+  }
+  const nomeAnimal = animal.specie;
+  const especie = species.find((elemento) => elemento.name === nomeAnimal);
+  const entradas = Object.keys(animal);
+  const quantPorSexo = especie.residents.reduce((acc, element) => {
+    if (element.sex === animal.sex) {
+      return acc + 1;
+    }
+    return acc;
+  }, 0);
+  if (entradas.length === 2) {
+    return quantPorSexo;
+  }
+  return todos[nomeAnimal];
 }
 
 module.exports = countAnimals;
