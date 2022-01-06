1773a2958cf5a1610fd531f322c3c062532f892d
Rachel Tairum <rachel.tairum@gmail.com>
2022-01-06 18:22:15 -0300

    Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  const vazio = {};
+
+  const funcionario = employees.find(
+    (pessoa) => pessoa.firstName === employeeName || pessoa.lastName === employeeName,
+  );
+
+  if (employeeName === undefined) {
+    return vazio;
+  }
+  return funcionario;
 }
 
+console.log(getEmployeeByName('Wishart'));
 module.exports = getEmployeeByName;
