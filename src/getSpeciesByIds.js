9e0ff749f6d0c2655c344ec59db086b7204108bb
Rachel Tairum <rachel.tairum@gmail.com>
2022-01-06 15:52:01 -0300

    Requisito 1 - Completo

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  return data.species.filter((element2) => element2.id === ids);
+function getSpeciesByIds(...ids) {
+  return ids.reduce((acumulador, elemento) => {
+    const selecao = data.species.filter((element2) => element2.id === elemento);
+    const achado = selecao[0];
+    acumulador.push(achado);
+    return acumulador;
+  }, []);
 }
 
 module.exports = getSpeciesByIds;
