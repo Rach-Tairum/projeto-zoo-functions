08c742e0dbafe26c11b3b7a46eaa8cfdff1f2dbc
Rachel Tairum <rachel.tairum@gmail.com>
2022-01-06 15:39:34 -0300

    Requisito 1 - Certo com 1 id

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,7 @@
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(ids) {
-  // seu código aqui
+  return data.species.filter((element2) => element2.id === ids);
 }
 
 module.exports = getSpeciesByIds;
