1bcbff9dfd5389f673db0abd9c3bfc82beb859d9
Rachel Tairum <rachel.tairum@gmail.com>
2022-01-07 19:38:33 -0300

    Requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,22 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const criança = entrants.filter((elemento) => elemento.age < 18);
+  const adultos = entrants.filter((elemento) => elemento.age >= 18 && elemento.age < 50);
+  const idosos = entrants.filter((elemento) => elemento.age >= 50);
+
+  return { adult: adultos.length, child: criança.length, senior: idosos.length };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!Array.isArray(entrants)) return 0;
+  const visitantes = countEntrants(entrants);
+  const { adult: adultos, child: crianca, senior: idosos } = visitantes;
+  const { prices } = data;
+  const { adult, senior, child } = prices;
+
+  const valorTotal = (adult * adultos) + (senior * idosos) + (child * crianca);
+  return valorTotal;
 }
 
 module.exports = { calculateEntry, countEntrants };
