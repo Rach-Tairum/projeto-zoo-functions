078d7bdd85af9c9f610cfab49e9dab15b893a0f2
Rachel Tairum <rachel.tairum@gmail.com>
2022-01-07 16:18:29 -0300

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,16 @@
-const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
+/* Grupo de estudos no dia 07/01/2022 */
 function isManager(id) {
-  // seu código aqui
+  return employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const manEmployees = employees.filter(({ managers }) => managers.includes(managerId));
+  return manEmployees.map(({ firstName, lastName }) => `${firstName} ${lastName}`);
 }
 
 module.exports = { isManager, getRelatedEmployees };
