a0fcd00819a48d6783744603aba3ef3f70f3c644
Rachel Tairum <rachel.tairum@gmail.com>
2022-01-07 21:26:15 -0300

    Requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const { employees } = data;
+  const primeiroAnimalResponsavel = employees.find((pessoa) => pessoa.id === id).responsibleFor[0];
+
+  const { species } = data;
+  const especie = species.find((animal) => animal.id === primeiroAnimalResponsavel);
+  const especieAnimais = especie.residents;
+
+  const animalMaisVelho = especieAnimais.reduce((acc, nome) => {
+    if (acc.age > nome.age) {
+      return acc;
+    }
+    return nome;
+  }, {});
+
+  const { age, name, sex } = animalMaisVelho;
+  const resultado = [name, sex, age];
+  return resultado;
 }
 
 module.exports = getOldestFromFirstSpecies;
