dd4f77d9637a3f6f2fe514e011159a4ee2e34472
Rachel Tairum <rachel.tairum@gmail.com>
2022-01-09 12:28:52 -0300

    Requisito 7 com 3 testes passando

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,47 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+function animalRegiao(local) {
+  const localizacao = species.filter((animalLocal) => animalLocal.location === local);
+  const nomeAnimal = localizacao.map((elemento) => elemento.name);
+  return nomeAnimal;
 }
 
+function animalNames(animal) {
+  const primeiroPasso = species.find((element) => element.name === animal);
+  const { residents } = primeiroPasso;
+  const segundoPasso = residents.map((elemento) => elemento.name);
+  return segundoPasso;
+}
+
+function animaisComNome() {
+  return species.reduce((acc, local) => {
+    const obj = animalRegiao(local.location).map(
+      (elemento) => ({ [elemento]: animalNames(elemento) }),
+    );
+    return ({ ...acc, [local.location]: obj });
+  }, {});
+}
+
+function animaisComNomeOrdenado() {
+  return species.reduce((acc, local) => {
+    const obj = animalRegiao(local.location).map(
+      (elemento) => ({ [elemento]: animalNames(elemento).sort() }),
+    );
+    return ({ ...acc, [local.location]: obj });
+  }, {});
+}
+console.log(animaisComNomeOrdenado().NE)
+
+function getAnimalMap(options) {
+  if (options === undefined || !options.includeNames) {
+    const animaisPorRegiao = species.reduce(
+      (acc, local) => ({ ...acc, [local.location]: animalRegiao(local.location) }), {},
+    );
+    return animaisPorRegiao;
+  }
+  if (options.includeNames) return animaisComNome();
+  if (options.includeNames && options.sorted) return animaisComNomeOrdenado();
+}
+
+console.log(getAnimalMap({ includeNames: true, sorted: true }));
 module.exports = getAnimalMap;
