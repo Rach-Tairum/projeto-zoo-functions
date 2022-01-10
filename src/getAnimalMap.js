912b698c8e4860ce3d64a03fb95077105616fe03
Rachel Tairum <rachel.tairum@gmail.com>
2022-01-10 19:29:00 -0300

    Requisito 7 - Completo

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -6,14 +6,30 @@ function animalRegiao(local) {
   return nomeAnimal;
 }
 
+function animaisPorRegiao() {
+  const animaisRegiao = species.reduce(
+    (acc, local) => ({ ...acc, [local.location]: animalRegiao(local.location) }), {},
+  );
+  return animaisRegiao;
+}
+// fim da montagem do objeto inicial, que tem localização e nomes dos animais naquele local
+
 function animalNames(animal) {
   const primeiroPasso = species.find((element) => element.name === animal);
   const { residents } = primeiroPasso;
-  const segundoPasso = residents.reduce((acc, elemento) => [...acc, elemento.name], []);
+  const segundoPasso = residents.map((element) => element.name);
   return segundoPasso;
 }
 
-function animaisComNomePorRegiao() {
+function animaisComNomePorRegiao(options) {
+  if (options.sorted) {
+    return species.reduce((acc, local) => {
+      const obj = animalRegiao(local.location).map(
+        (elemento) => ({ [elemento]: animalNames(elemento).sort() }),
+      );
+      return ({ ...acc, [local.location]: obj });
+    }, {});
+  }
   return species.reduce((acc, local) => {
     const obj = animalRegiao(local.location).map(
       (elemento) => ({ [elemento]: animalNames(elemento) }),
@@ -22,28 +38,43 @@ function animaisComNomePorRegiao() {
   }, {});
 }
 
-console.log(animaisComNomePorRegiao());
+// fim do desenvolvimento do objeto que possui a regiao, os animais pertencentes a ela com os nomes dos animais que constam no zoo.
 
-function animaisComNomeOrdenado() {
+function animaisPorSexo(animais, sexo) {
+  const primeiroPasso = species.find((element) => element.name === animais);
+  const { residents } = primeiroPasso;
+  const segundoPasso = residents.filter((element) => element.sex === sexo);
+  const terceiroPasso = segundoPasso.map((element) => element.name);
+  return terceiroPasso;
+}
+
+function animaisComNomePorSexo(animalSex, options) {
+  if (options.sorted === true) {
+    return species.reduce((acc, local) => {
+      const obj = animalRegiao(local.location).map(
+        (elemento) => ({ [elemento]: animaisPorSexo(elemento, animalSex).sort() }),
+      );
+      return ({ ...acc, [local.location]: obj });
+    }, {});
+  }
   return species.reduce((acc, local) => {
     const obj = animalRegiao(local.location).map(
-      (elemento) => ({ [elemento]: animalNames(elemento).sort() }),
+      (elemento) => ({ [elemento]: animaisPorSexo(elemento, animalSex) }),
     );
     return ({ ...acc, [local.location]: obj });
   }, {});
 }
-console.log(animaisComNomeOrdenado());
+// fim do desenvolvimento para conseguir o nome do animal por sexo
 
-function getAnimalMap(options) {
-//   if (options === undefined || !options.includeNames) {
-//     const animaisPorRegiao = species.reduce(
-//       (acc, local) => ({ ...acc, [local.location]: animalRegiao(local.location) }), {},
-//     );
-//     return animaisPorRegiao;
-//   }
-//   if (options.includeNames) return animaisComNomePorRegiao();
-//   if (options.includeNames && options.sorted) return animaisComNomeOrdenado();
+function test(options) {
+  if (options.includeNames && options.sex) return animaisComNomePorSexo(options.sex, options);
+  if (options.includeNames) return animaisComNomePorRegiao(options);
 }
 
-// console.log(getAnimalMap({ includeNames: true }));
+function getAnimalMap(options) {
+  if (options === undefined || !options.includeNames) return animaisPorRegiao();
+
+  return test(options);
+}
+
 module.exports = getAnimalMap;
